1313b77ac8098bef6ff4bc4d33c9f8539cb65b0b
Nikoloz Khmaladze <nkhmaladze.official@gmail.com>
2024-07-31 17:04:32 -0400

    error 500

--- server/src/getPrice.js
+++ server/src/getPrice.js
@@ -2,7 +2,7 @@ const axios = require("axios")
 
 const APP_ID = process.env.APP_ID_EBAY
 
-const url = "https://svcs.ebay.com/services/search/FindingService/v1"
+const url = "https://svcs.sandbox.ebay.com/services/search/FindingService/v1"
 
 const params = {
     'OPERATION-NAME': 'findItemsByKeywords',
@@ -10,25 +10,27 @@ const params = {
     'SECURITY-APPNAME': APP_ID,
     'RESPONSE-DATA-FORMAT': 'JSON',
     'keywords': 'laptop', //name of product here
-    'paginationInput.entriesPerPage':1
 }
 
-axios.getAdapter(url, { params })
+//console.log('Request URL:', url);
+//console.log('Request Params:', params);
+
+axios.get(url, { params })
     .then(response =>{
         const data = response.data
-        const items = data.findItemsByKeywordsResponse[0].searchResult[0].items
+        const items = data.findItemsByKeywordsResponse[0].searchResult[0].item
 
         if (items && items.length >0){
             const item = items[0]
             const title = item.title[0]
             const price = item.sellingStatus[0].currentPrice[0].__value__
             const currency = item.sellingStatus[0].currentPrice[0]['@currencyId']
-            console.log('Title: ${title}')
-            console.log('Price: ${price} ${currency}')
-        } else {
+            console.log(`Title: ${title}`)
+            console.log(`Price: ${price} ${currency}`)
+        }else {
             console.log('No items found.')
         }
     })
     .catch(error => {
-        console.error('Request failed: ${error.message}')
+        console.error(`Request failed: ${error.message}`)
     })
